1a4a44501bd6c19f9a6194a6887cbea5e5b54d5b
foxccs <142402202+foxccs@users.noreply.github.com>
2025-05-16 21:00:36 +0800

    Update Setting.vue

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -15,13 +15,13 @@
   <div>
     <p style="color: red">无云端功能，本次构建版本为v3.0.1。</p>
     <h4>作者：</h4>
-    <p>ZhangDi，https://git.io/zd，<br>群 1065753778</p>
+    <p>狐狸导航，www.foxccs.cn，<br>群 946392970</p>
     <h4>项目介绍：</h4>
     <p>本项目使用Vue3构建，推荐使用Chrome内核浏览器。</p>
     <h4>项目地址：</h4>
-    <p>https://github.com/zzd/Simple-Search-Page</p>
+    <p>www.foxccs.cn</p>
     <h4>BUG反馈：</h4>
-    <a href="https://github.com/zzd/Simple-Search-Page/issues/new/choose">Github issues</a>
+    <a href="https://www.foxccs.cn/">狐狸导航</a>
     <h4>版权声明：</h4>
     <p>本项目开源。</p></div>
 </template>
